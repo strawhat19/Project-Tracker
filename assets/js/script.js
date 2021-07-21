249081b0706116d6f96908b4e6128e5e0d651c54
Rakib Ahmed <rakib987@gmail.com>
2021-07-20 21:11:30 -0400

    Changes to style

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+var updateTime = setInterval(function() {
+    var date = moment().format('MMM DD, YYYY');
+    var time = moment().format('hh:mm:ss a');
+    var dateElement = $('#date');
+    dateElement.html(date + ' at ' + time);
+}, 1000)
+
+var popupButton = $('#popupForm');
+popupButton.on('click', function(event) {
+
+})
+
+
+
+
+
