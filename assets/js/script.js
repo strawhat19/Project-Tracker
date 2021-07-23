e767f49b0da067750c39404dc90b199848a8c658
Rakib Ahmed <rakib987@gmail.com>
2021-07-23 03:56:43 -0400

    Styling Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-var updateTime = setInterval(function() {
+var updateTime = setInterval(updatedTime = () => {
+    var dateElement = $('#date');
     var date = moment().format('MMM DD, YYYY');
     var time = moment().format('hh:mm:ss a');
-    var dateElement = $('#date');
     dateElement.html(date + ' at ' + time);
 }, 1000)
 
